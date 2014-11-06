0ee6e888f2172697470de884c1fbb5f14c68e175
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-11-06 23:34:45 +0100

    Ajout d'une indication de liscence

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+/*
+	PROGRAMME :
+		AG44LevelGraph
+	DEVELOPPEUR :
+		Antoine LAMIELLE <antoine.lamielle@utbm.fr>
+	PROJECT START DATE :
+		September, 19th 2014
+	LICENCE :
+		AG44LevelGraph is a project realised as a part of the AG44-lessons at the UTBM school, dealing with Graph theory.
+		Copyright © 2014 Antoine LAMIELLE
+
+		This program is free software; you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation; either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License along
+		with this program; if not, write to the Free Software Foundation, Inc.,
+		51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
